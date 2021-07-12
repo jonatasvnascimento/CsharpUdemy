83243cc3c0e59ca4cf72ff9e3afa35eeee652225
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-07-12 18:14:04 -0300

    Coment

---
+++ Comentarios/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comentarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //teste code
+            
+            int v = 10000000;
+            Console.WriteLine($"{v}");
+            Console.ReadKey();
+            
+        }
+    }
+}

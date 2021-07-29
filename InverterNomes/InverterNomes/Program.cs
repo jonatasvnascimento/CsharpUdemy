7fa8120c260eb5a0d2b5da61a15dbad92c4d2b3b
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-07-29 16:34:18 -0300

    termino aula invert name

--- InverterNomes/InverterNomes/Program.cs
+++ InverterNomes/InverterNomes/Program.cs
@@ -8,14 +8,23 @@ namespace InverterNomes
         {
             string nome1, nome2, nome3, nome4, aux;
 
-            Console.Write("Digite o primeiro 1 nome");
+            Console.Write("Digite o primeiro 1 nome: ");
             nome1 = Console.ReadLine();
-            Console.Write("Digite o primeiro 2 nome");
+            Console.Write("Digite o primeiro 2 nome: ");
             nome2 = Console.ReadLine();
-            Console.Write("Digite o primeiro 3 nome");
+            Console.Write("Digite o primeiro 3 nome: ");
             nome3 = Console.ReadLine();
-            Console.Write("Digite o primeiro 4 nome");
+            Console.Write("Digite o primeiro 4 nome: ");
             nome4 = Console.ReadLine();
+
+            aux = nome1;
+            nome1 = nome4;
+            nome4 = aux;
+            aux = nome2;
+            nome2 = nome3;
+            nome3 = aux;
+
+
 
             Console.WriteLine($"{nome1} {nome2} {nome3} {nome4}");
         }

b3b66de4130931b81639ef25f446e96c7cad1d29
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-07-29 15:32:53 -0300

    ConvertTemperature

---
+++ InverterNomes/InverterNomes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InverterNomes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome1, nome2, nome3, nome4, aux;
+
+            Console.Write("Digite o primeiro 1 nome");
+            nome1 = Console.ReadLine();
+            Console.Write("Digite o primeiro 2 nome");
+            nome2 = Console.ReadLine();
+            Console.Write("Digite o primeiro 3 nome");
+            nome3 = Console.ReadLine();
+            Console.Write("Digite o primeiro 4 nome");
+            nome4 = Console.ReadLine();
+
+            Console.WriteLine($"{nome1} {nome2} {nome3} {nome4}");
+        }
+    }
+}
---
+++ TemperatureConverter/TemperatureConverter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TemperatureConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Celsius, Kelvin, Fahrenheit, Rankine = 0;
+
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("      Conversor de temperatura");
+            Console.WriteLine("---------------------------------------");
+
+            Console.Write("Informe a temperatura em Celsius: ");
+            Celsius = Convert.ToDouble(Console.ReadLine());
+
+            Fahrenheit = (Celsius * 1.8) + 32;
+            Kelvin = Celsius + 273.15;
+            Rankine = (Celsius * 1.8) + 491.67;
+
+            Console.WriteLine("Fahrenheit: F° {0:f2}", Fahrenheit);
+            Console.WriteLine("Kelvin:     K° {0:f2}", Kelvin);
+            Console.WriteLine("Rankine:    R° {0:f2}", Rankine);
+            Console.WriteLine("Celsius:    C° {0:f2}", Celsius);
+
+        }
+    }
+}

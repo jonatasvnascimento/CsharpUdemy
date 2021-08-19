0621026d83f0ccd9a1fe5a5370bfc5e2d0ca58ea
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-08-18 21:30:42 -0300

    CalculatorIMC

---
+++ Aulas/Condicionais/Condicionais/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        inicio:
+
+            Console.Write("Operador: ");
+            string opreacao = Console.ReadLine();
+            string result = "";
+
+            if (opreacao == "+" ||
+                opreacao == "*" ||
+                opreacao == "/" ||
+                opreacao == "-")
+            {
+                result = opreacao;
+            }
+            else
+            {
+                goto inicio;
+            }
+
+            Console.Write("Informe o primeiro numero: ");
+            int v1 = int.Parse(Console.ReadLine());
+            Console.Write("Informe o segundo numero: ");
+            int v2 = int.Parse(Console.ReadLine());
+            int soma = v1 + v2;
+
+            if (result == "+")
+            {
+                int mais = v1 + v2;
+                Console.WriteLine($"{mais}");
+
+                if (mais % 2 == 0)
+                {
+                    Console.Write("Numero e par");
+                }
+                else
+                {
+                    Console.WriteLine("Numero não e par");
+                }
+            }
+            else if (result == "-")
+            {
+                int menos = v1 - v2;
+                Console.WriteLine($"{menos}");
+
+                string veric = menos % 2 == 0 ? "Par" : "Impar";
+
+                Console.WriteLine($"{veric}");
+
+            }
+            else if (result == "*")
+            {
+                int multiplic = v1 * v2;
+                Console.WriteLine($"{multiplic}");
+                string veric = multiplic % 2 == 0 ? "Par" : "Impar";
+                Console.WriteLine($"{veric}");
+
+            }
+            else if (result == "/")
+            {
+                Console.WriteLine($"{v1 / v2}");
+            }
+
+            Console.Write("Deseja continuar: [s/n] ");
+            string resp = Console.ReadLine();
+
+            if (resp == "S" || resp == "s")
+            {
+                goto inicio;
+            }
+
+        }
+    }
+}
---
+++ Projetos/CalculatorIMC/CalculatorIMC/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+
+namespace CalculatorIMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------- Calculo do IMC ----------------------");
+
+            double
+                pesoFinal = 0,
+                alturaFinal = 0,
+                imc = 0;
+
+            string peso = "";
+            string altura = "";
+            string resp = "";
+
+        informePeso:
+
+            Console.Write("Informe o peso: ");
+            peso = Console.ReadLine();
+
+            if (peso == "" || !peso.All(char.IsDigit))
+            {
+                Console.WriteLine("Peso invalido");
+                goto informePeso;
+            }
+            else
+            {
+                pesoFinal = double.Parse(peso);
+            }
+
+        imformeAltura:
+
+            Console.Write("Informe a altura: ");
+            altura = Console.ReadLine();
+            if (altura == "")
+            {
+                Console.WriteLine("Altura invalida");
+                goto imformeAltura;
+
+            }
+            else
+            {
+                try
+                {
+                alturaFinal = double.Parse(altura);
+                }
+                catch
+                {
+                    Console.WriteLine("Altura invalida");
+                    goto imformeAltura;
+                }
+            }
+           
+            imc = pesoFinal / (alturaFinal * alturaFinal);
+
+            if (imc < 18.5)
+            {
+                Console.WriteLine($"{Math.Round(imc, 1)}: Abaixo do Peso");
+            }else if (imc <= 18.5 && imc <= 25)
+            {
+                Console.WriteLine($"{Math.Round(imc, 1)}: No peso normal");
+            }else if (imc <=25 && imc <=30)
+            {
+                Console.WriteLine($"{Math.Round(imc, 1)}: Acima do peso");
+            }
+            else
+            {
+                Console.WriteLine($"{Math.Round(imc, 1)}: Obeso");
+            }
+
+            Console.WriteLine("Deseja continuar? [s/n] ");
+            resp = Console.ReadLine();
+            if (resp == "S" || resp == "s" )
+            {
+                Console.Clear();
+                goto informePeso;
+            }
+           
+            //Console.WriteLine($"{Math.Round(imc, 1)}");
+
+
+        }
+    }
+}

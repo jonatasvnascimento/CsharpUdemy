b1912fac862cc71804a4771855d678854046b000
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2021-12-27 13:07:22 -0300

    61. Utilizando o termo this

---
+++ Aulas/Secao-12/61. Utilizando o termo this/Acessar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _61._Utilizando_o_termo_this
+{
+    internal class Acessar
+    {
+        string Senha = "abc123";
+        public bool Login(string paramSenha)
+        {
+           return this.Senha == paramSenha;
+        }
+    }
+}
---
+++ Aulas/Secao-12/61. Utilizando o termo this/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _61._Utilizando_o_termo_this
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Acessar acessar = new Acessar();
+            if (acessar.Login("abc123"))
+            {
+                Console.WriteLine("Acesso liberado");
+            }
+            else
+            {
+                Console.WriteLine("Senha invalida");
+            }
+        }
+    }
+}

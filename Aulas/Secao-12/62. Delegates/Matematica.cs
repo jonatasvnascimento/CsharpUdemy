881121ccee0ff34a4fc6f05bc2b05560d9404af3
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-01-02 11:23:44 -0300

    62

---
+++ Aulas/Secao-12/62. Delegates/Matematica.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _62._Delegates
+{
+    internal class Matematica
+    {
+        public void somar(int v1, int v2)
+        {
+            int soma = v1 + v2;
+            Console.WriteLine($"A soma de {v1} + {v2} = {soma}");
+        }
+        public void subitrair(int v1, int v2)
+        {
+            int sub = v1 - v2;
+            Console.WriteLine($"A subtração de {v1} - {v2} = {sub}");
+        }
+        public void multiplicar(int v1, int v2)
+        {
+            int mult = v1 * v2;
+            Console.WriteLine($"A soma de {v1} * {v2} = {mult}");
+        }
+        public void dividir(int v1, int v2)
+        {
+            int div = v1 / v2;
+            Console.WriteLine($"A subtração de {v1} / {v2} = {div}");
+        }
+    }
+}
---
+++ Aulas/Secao-12/62. Delegates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _62._Delegates
+{
+    internal class Program
+    {
+        delegate void Operacao(int n1, int n2);
+        static void Main(string[] args)
+        {
+            Matematica modulo = new Matematica();
+            Operacao operacao = null;
+
+            operacao += modulo.somar;
+            operacao += modulo.subitrair;
+            operacao += modulo.dividir;
+            operacao += modulo.multiplicar;
+
+            operacao(2, 2);
+            
+            operacao -= modulo.multiplicar;
+            operacao(2, 2);
+
+
+
+
+        }
+    }
+}

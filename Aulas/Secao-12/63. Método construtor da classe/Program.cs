83722d2a1dcca79cfb0053d8b4943a545a53bca9
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-01-02 11:26:33 -0300

    63

---
+++ Aulas/Secao-12/63. Método construtor da classe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _63._Método_construtor_da_classe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

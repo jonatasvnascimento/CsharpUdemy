d8a55db0b3dbcf4306352467c1da0d9736d64a92
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-01-03 16:31:18 -0300

    64

---
+++ Aulas/Secao-12/64. Sobrecarga de construtor (Overloading)/Pessoa.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _64._Sobrecarga_de_construtor__Overloading_
+{
+    
+    internal class Pessoa
+    {
+        public string nome;
+        public string sobrenome;
+        public int idade;
+        public int anoNascimento;
+
+        public Pessoa()
+        {
+            nome = "Vazio";
+            sobrenome = "Vazio";
+            anoNascimento = 0;
+            idade = 0;
+        }
+        public Pessoa(string _nome, string _sobrenome)
+        {
+            this.nome = _nome;
+            this.sobrenome= _sobrenome;
+            anoNascimento = 0;
+            idade = 0;
+        }
+        public Pessoa(string _nome, string _sobrenome, int _anoNascimento, int _idade)
+        {
+            this.nome = _nome;
+            this.sobrenome= _sobrenome;
+            this.anoNascimento = _anoNascimento;
+            this.idade = Idade();
+        }
+        private int Idade()
+        {
+            return 2022 - anoNascimento;
+        }
+    }
+}
--- Aulas/Secao-12/64. Sobrecarga de construtor (Overloading)/Program.cs
+++ Aulas/Secao-12/64. Sobrecarga de construtor (Overloading)/Program.cs
@@ -6,7 +6,12 @@ namespace _64._Sobrecarga_de_construtor__Overloading_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pessoa p2 = new Pessoa("Player","02",1900,0);
+            Console.WriteLine(
+                $"nome:..........{p2.nome}\n" +
+                $"sobrenome:.....{p2.sobrenome}\n" +
+                $"idade:.........{p2.idade}\n" +
+                $"anoNascimento: {p2.anoNascimento}");
         }
     }
 }

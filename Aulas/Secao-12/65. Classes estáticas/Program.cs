f8509d6d55ae9238a34e2de9e3bff38aa18bfb78
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-01-04 13:15:35 -0300

    65

---
+++ Aulas/Secao-12/65. Classes estáticas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _65._Classes_estáticas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

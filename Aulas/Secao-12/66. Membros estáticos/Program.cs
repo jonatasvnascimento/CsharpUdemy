8a5ed05c48488a460b9ea7f66e78030ed261eac6
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-01-05 11:31:35 -0300

    65

---
+++ Aulas/Secao-12/66. Membros estáticos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _66._Membros_estáticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

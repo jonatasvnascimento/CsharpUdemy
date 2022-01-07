42e2448c2e006b4a9d9bdbbf7b2547cb83e508e4
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-01-07 07:47:21 -0300

    67

---
+++ Aulas/Secao-12/67. Classes parciais/PessoaMetodos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _67._Classes_parciais
+{
+    partial class Pessoa
+    {
+        public void Apresentar()
+        {
+            Console.WriteLine($"Ola eu sou:{nome}");
+        }
+        public static int CalcularIdade(int anoNasc)
+        {
+            return DateTime.Now.Year - anoNasc;
+        }
+    }
+}
---
+++ Aulas/Secao-12/67. Classes parciais/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _67._Classes_parciais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa.maioridade = 24;
+            Pessoa p1 = new Pessoa();
+            p1.nome = "Player";
+            p1.idade = 18;
+            Console.WriteLine(
+                $"{Pessoa.CalcularIdade(1950)}" +
+                $"{p1.idade}" +
+                $"{p1.nome}");
+        }
+    }
+}

f4a0ec0c8bd5dd34bd11d052f4a40d615838a03a
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-01-10 07:32:57 -0300

    68

---
+++ Aulas/Secao-12/68. Propriedades da classe (get - set)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _68._Propriedades_da_classe__get___set_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

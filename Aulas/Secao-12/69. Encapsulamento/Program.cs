58d6a338affdfe3891641f44df47b220391bb805
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-01-12 08:40:07 -0300

    69

---
+++ Aulas/Secao-12/69. Encapsulamento/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _69._Encapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c = new Conta();
+            c.Cliente = "Jonnes";
+
+            double valorDeposit = 100;
+            c.Depositar(valorDeposit);
+
+            double valorSacar = 100;
+            c.Sacar(valorSacar);
+
+            Console.WriteLine(
+                $"Cliente:{c.Cliente}\n" +
+                $"Saldo:..{c.Saldo}");
+        }
+    }
+}

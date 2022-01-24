dd3822efbd62e17b41d5a156fe985dfa53d1d642
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-01-24 17:31:11 -0300

    73

---
+++ Aulas/Secao-12/73. Membros sealed/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _73._Membros_sealed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

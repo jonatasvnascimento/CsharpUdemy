9de112ba7a94b20e55de83ac9f28e0a845eb589a
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-01-24 17:30:31 -0300

    72

---
+++ Aulas/Secao-12/72. Classes sealed/Bicicleta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _72._Classes_sealed
+{
+    sealed internal class Bicicleta : Veiculo
+    {
+        public override void Acelerar()
+        {
+            throw new NotImplementedException();
+        }
+        public override void Parar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

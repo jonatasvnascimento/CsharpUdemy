cbb193569c8526d8e7fbae47a614358504bd00ee
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-01-25 10:32:00 -0300

    74

---
+++ Aulas/Secao-12/74. Modificadores de acesso/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _74._Modificadores_de_acesso
+{
+    internal class Teste
+    {
+        private string nome;
+        public string sobrenome;
+
+        private void Metodo1() { }
+        public void Executar() { }
+    }
+    class Humano
+    {
+        protected string nome;
+        private string sobrenome;
+        internal int idade;
+    }
+    class Homem : Humano
+    {
+        public void Executar() {
+            nome = "";
+        }
+    }
+}
---
+++ Aulas/Secao-12/74. Modificadores de acesso/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _74._Modificadores_de_acesso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Teste t = new Teste();
+            t.sobrenome = "";
+            t.Executar();
+        }
+    }
+}

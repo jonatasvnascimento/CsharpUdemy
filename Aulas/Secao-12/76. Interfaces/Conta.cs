5000a72499ea00b92ae3771c63ecc6cf2274b704
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-01-26 12:06:22 -0300

    76

---
+++ Aulas/Secao-12/76. Interfaces/Conta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _76._Interfaces
+{
+    internal class CaixaEletronico : IConta
+    {
+        public string Usuatio { get; set; }
+
+        public void Depositar()
+        {
+        }
+
+        public void Sacar()
+        {
+        }
+
+        public void Saldo()
+        {
+        }
+
+        public void SolicitarInprestimo()
+        {
+        }
+    }
+
+    interface IConta
+    {
+        string Usuatio { get; set; }
+        void Depositar();
+        void Sacar();
+        void Saldo();
+    }
+}
---
+++ Aulas/Secao-12/76. Interfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _76._Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

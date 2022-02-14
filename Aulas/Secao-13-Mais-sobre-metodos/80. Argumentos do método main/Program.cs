088ee63b765b14e318f15e380288e3090812b29d
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-02-14 15:18:25 -0300

    _80._Argumentos_do_método_main

---
+++ Aulas/Secao-13-Mais-sobre-metodos/80. Argumentos do método main/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _80._Argumentos_do_método_main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = args[0];
+            string senha = args[1];
+            string senhaADM = "123";
+
+            if (senha == senhaADM && nome == "admin")
+            {
+                Console.WriteLine($"Bem vindo{nome}");
+            }
+            else
+            {
+                Console.WriteLine("Senha informada invalida");
+            }
+
+        }
+    }
+}

d02e68d9c83ae74f76f334f70a416944e305eae6
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-02-14 17:13:09 -0300

    _81_FatorialRecursividade

---
+++ Projetos/81_FatorialRecursividade/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _81_FatorialRecursividade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Fatorial: ");
+            
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Fatorial de {num}! = {fat(num)}");
+
+           static int fat(int num)
+            {
+                if (num <= 0)
+                {
+                    return 1;
+                }
+                return num * fat(num - 1);
+            }
+        }
+    }
+}

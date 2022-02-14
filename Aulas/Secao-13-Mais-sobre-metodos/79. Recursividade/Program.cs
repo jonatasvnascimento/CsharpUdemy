05070d32f97b9bef22a2955c9f76c87d02f65d62
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2022-02-14 10:15:21 -0300

    _79._Recursividade

---
+++ Aulas/Secao-13-Mais-sobre-metodos/79. Recursividade/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _79._Recursividade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Recursiva r = new Recursiva();
+            r.ExecutarR("Teste", 10);
+        }
+    }
+}
---
+++ Aulas/Secao-13-Mais-sobre-metodos/79. Recursividade/Recursiva.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _79._Recursividade
+{
+    internal class Recursiva
+    {
+        public void Executar(string msg, int loop)
+        {
+            for (int i = 0; i< loop; i++)
+            {
+                Console.WriteLine(msg);
+            }
+        }
+
+        public void ExecutarR(string msg, int loop)
+        {
+            if (loop > 0)
+            {
+                Console.WriteLine(msg);
+                Executar(msg, loop - 1);
+            }
+           
+        }
+    }
+}
